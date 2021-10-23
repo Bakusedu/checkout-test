2e6eedd26ea9fae9338de225ec1e4d7e998b813a
Chiedu <iamnedu912@gmail.com>
2021-10-23 20:49:03 +0100

    added more fixes

--- checkout/klump.js
+++ checkout/klump.js
@@ -18,7 +18,7 @@ class KlumpPopUp {
                 'https://elastic-kilby-0d4f88.netlify.app'
             );
             iFrame.style.cssText =
-                '\nbackground: transparent;\nbackground: rgba(0,0,0,0.005);\nborder: 0px none transparent;\noverflow-x: hidden;\noverflow-y: hidden;\nvisibility: hidden;\nmargin: 0;\npadding: 0;\n-webkit-tap-highlight-color: transparent;\n-webkit-touch-callout: none; position: fixed;\nleft: 0;\ntop: 0;\nwidth: 100%;\nheight: 100%;)';
+                '\nbackground: transparent;\nbackground: rgba(0,0,0,0.005);\nborder: 0px none transparent;\noverflow-x: hidden;\noverflow-y: hidden;\nmargin: 0;\npadding: 0;\n-webkit-tap-highlight-color: transparent;\n-webkit-touch-callout: none; position: fixed;\nleft: 0;\ntop: 0;\nwidth: 100%;\nheight: 100%;)';
             body.appendChild(iFrame);
         }
     }
