bcc4117e5e6488e2c07e6dd1519b6a88a0d5d4a5
Chiedu <iamnedu912@gmail.com>
2021-10-23 20:26:12 +0100

    added payment test logic

--- checkout/klump.js
+++ checkout/klump.js
@@ -3,3 +3,27 @@ form.addEventListener('submit', (event) => {
     alert('form has been submitted');
     event.preventDefault();
 });
+
+class KlumpPopUp {
+    email = null;
+    amount = null;
+    flag = false;
+    key = '123';
+    constructor(public_key, email, amount) {
+        this.flag = this.key === public_key ? true : false;
+        this.email = email;
+        this.amount = amount;
+    }
+
+    payWithKlump() {
+        if (this.flag && this.email && this.amount) {
+            const body = document.getElementsByTagName('body')[0];
+            const iFrame = document.createElement('iframe');
+            iFrame.setAttribute(
+                'src',
+                'https://elastic-kilby-0d4f88.netlify.app'
+            );
+            body.appendChild(iFrame);
+        }
+    }
+}
