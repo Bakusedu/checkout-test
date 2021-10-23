e288727020153f0507cc55c95773b1d75b933e2b
Chiedu <iamnedu912@gmail.com>
2021-10-23 20:34:57 +0100

    added more fixes

--- checkout/klump.js
+++ checkout/klump.js
@@ -1,9 +1,3 @@
-const form = document.getElementById('checkout');
-form.addEventListener('submit', (event) => {
-    alert('form has been submitted');
-    event.preventDefault();
-});
-
 class KlumpPopUp {
     email = null;
     amount = null;
