b08690cb2f9c352e352b54479ee76b2b32fb19b5
Chiedu <iamnedu912@gmail.com>
2021-10-23 21:10:31 +0100

    added more fixes

--- checkout/klump.js
+++ checkout/klump.js
@@ -1,16 +1,14 @@
 class KlumpPopUp {
-    email = null;
     amount = null;
     flag = false;
     key = '123';
-    constructor(public_key, email, amount) {
+    constructor(public_key, amount) {
         this.flag = this.key === public_key ? true : false;
-        this.email = email;
         this.amount = amount;
     }
 
     payWithKlump() {
-        if (this.flag && this.email && this.amount) {
+        if (this.flag && this.amount) {
             const body = document.getElementsByTagName('body')[0];
             const iFrame = document.createElement('iframe');
             iFrame.setAttribute(
@@ -19,6 +17,7 @@ class KlumpPopUp {
             );
             iFrame.style.cssText =
                 '\nbackground: transparent;\nbackground: rgba(0,0,0,0.005);\nborder: 0px none transparent;\noverflow-x: hidden;\noverflow-y: hidden;\nmargin: 0;\npadding: 0;\n-webkit-tap-highlight-color: transparent;\n-webkit-touch-callout: none; position: fixed;\nleft: 0;\ntop: 0;\nwidth: 100%;\nheight: 100%;)';
+            document.getElementById('payment').value = 'Pay NGN ' + this.amount;
             body.appendChild(iFrame);
         }
     }
