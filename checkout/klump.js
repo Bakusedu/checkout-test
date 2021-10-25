b2139e0d25ac7d7408fdd7e1ae0f7a9c87bbbad6
Chiedu <iamnedu912@gmail.com>
2021-10-25 20:37:33 +0100

    added more fixes

--- checkout/klump.js
+++ checkout/klump.js
@@ -8,27 +8,25 @@ class KlumpPopUp {
     payWithKlump() {
         if (this.flag) {
             const body = document.getElementsByTagName('body')[0];
-            this.iframe = document.createElement('iframe');
-            this.iframe.setAttribute(
+            const iFrame = document.createElement('iframe');
+            iFrame.setAttribute(
                 'src',
                 'https://elastic-kilby-0d4f88.netlify.app'
             );
-            this.iframe.style.cssText =
+            iFrame.style.cssText =
                 '\nbackground: transparent;\nbackground: rgba(0,0,0,0.005);\nborder: 0px none transparent;\noverflow-x: hidden;\noverflow-y: hidden;\nmargin: 0;\npadding: 0;\n-webkit-tap-highlight-color: transparent;\n-webkit-touch-callout: none; position: fixed;\nleft: 0;\ntop: 0;\nwidth: 100%;\nheight: 100%;)';
-            body.appendChild(this.iframe);
-            this.iframe.onload = function () {
-                this.iframe.contentWindow.postMessage('communication', '*');
-                this.setTransaction();
+            body.appendChild(iFrame);
+            iFrame.onload = function () {
+                iFrame.contentWindow.postMessage('communication', '*');
+                this.iframe = iFrame;
             };
         }
     }
-
-    setTransaction() {
-        window.addEventListener('message', function (event) {
-            console.log(
-                'button =' +
-                    this.iframe.contentDocument.getElementById('payment')
-            );
-        });
-    }
 }
+
+window.addEventListener('message', function (event) {
+    alert('received: ' + event.data);
+    console.log(this.iframe.contentDocument);
+
+    // can message back using event.source.postMessage(...)
+});
