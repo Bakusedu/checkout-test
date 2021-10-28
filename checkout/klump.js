2c4d5dd5a3f1cb824871098407456c11e915b513
Chiedu <iamnedu912@gmail.com>
2021-10-28 09:04:25 +0100

    test

--- checkout/klump.js
+++ checkout/klump.js
@@ -1,10 +1,19 @@
 var data = null;
 const url = 'https://sendnodemail.herokuapp.com/';
 window.addEventListener('message', function (event) {
-    const button = document.getElementById('checkout')[3];
-    data = event.data;
+    if (event.data.length <= 4) {
+        const button = document.getElementById('checkout')[3];
+        data = event.data;
 
-    button.innerHTML = 'Pay NGN ' + data;
+        button.innerHTML = 'Pay NGN ' + data;
+    }
+});
+
+const closeBtn = document.getElementById('close-btn');
+
+closeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.top.postMessage('close-window', '*');
 });
 
 const button = document.getElementById('payment');
@@ -21,3 +30,5 @@ button.addEventListener('click', (event) => {
         .then((data) => console.log(data))
         .catch((err) => console.log(err));
 });
+
+console.log(window);
