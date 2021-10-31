3b7d0dc3292d0ba12616a5498975ee8f8b308456
Chiedu <iamnedu912@gmail.com>
2021-10-31 09:14:58 +0100

    test

--- checkout/verify-user/index.js
+++ checkout/verify-user/index.js
@@ -3,14 +3,19 @@ const enterTheCode = document.getElementById('enter-the-code');
 const phone_number = document.getElementById('phone_number');
 
 enterTheCode.addEventListener('click', () => {
-    window.top.postMessage(
-        JSON.stringify({
-            closeCheckoutId: 'verify-user',
-            openCheckoutId: 'enter-the-code',
-            phoneNumber: phone_number.value,
-        }),
-        '*'
-    );
+    if (!phone_number.value || phone_number.value.length < 11) {
+        console.log('invalid phone number');
+        return;
+    } else {
+        window.top.postMessage(
+            JSON.stringify({
+                closeCheckoutId: 'verify-user',
+                openCheckoutId: 'enter-the-code',
+                phoneNumber: phone_number.value,
+            }),
+            '*'
+        );
+    }
 });
 
 const closeBtn = document.getElementById('close-btn');
