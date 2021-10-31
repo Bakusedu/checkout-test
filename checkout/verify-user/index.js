f7b749ce106bab8c527bb438f8d62db3960bcb43
Chiedu <iamnedu912@gmail.com>
2021-10-31 06:25:17 +0100

    added more demo forms

--- checkout/verify-user/index.js
+++ checkout/verify-user/index.js
@@ -1,10 +1,13 @@
 const enterTheCode = document.getElementById('enter-the-code');
 
+const phone_number = document.getElementById('phone_number');
+
 enterTheCode.addEventListener('click', () => {
     window.top.postMessage(
         JSON.stringify({
             closeCheckoutId: 'verify-user',
             openCheckoutId: 'enter-the-code',
+            phoneNumber: phone_number.value,
         }),
         '*'
     );
