ba0ae627872d9dfe8f544e9a4e4eeae46d2587b8
Chiedu <iamnedu912@gmail.com>
2021-10-31 18:36:20 +0100

    test

--- checkout/add-your-card/index.js
+++ checkout/add-your-card/index.js
@@ -4,6 +4,8 @@ var avatar = document.getElementById('avatar');
 
 var email = document.getElementById('email');
 
+var fullName = document.getElementById('fullName');
+
 closeBtn.addEventListener('click', () => {
     window.top.postMessage(
         JSON.stringify({
@@ -17,6 +19,7 @@ window.addEventListener('message', (event) => {
     const data = JSON.parse(event.data);
     console.log(data);
     console.log(data.user.fname.charAt[0]);
-    avatar.value = data.user.fname.charAt[0];
+    avatar.value = data.user.fname.charAt(0);
+    fullName.value = data.user.fname + ' ' + data.user.lname;
     email.value = data.user.email;
 });
