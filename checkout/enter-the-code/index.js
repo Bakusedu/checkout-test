92f15daf483dd40723da357b1d85b7727f213382
Chiedu <iamnedu912@gmail.com>
2021-10-31 07:36:11 +0100

    test

--- checkout/enter-the-code/index.js
+++ checkout/enter-the-code/index.js
@@ -1,10 +1,12 @@
-var message = document.getElementById('success-otp-alert');
+document.addEventListener('DOMContentLoaded', function () {
+    var message = document.getElementById('success-otp-alert');
 
-window.addEventListener('message', (event) => {
-    console.log(event);
-    const data = JSON.parse(event.data);
-    if (data.variable === 'phone_number') {
-        message.innerHTML = `A code has been sent to <strong>${data.phoneNumber}</strong> You
-                    should get it within 20 seconds`;
-    }
+    window.addEventListener('message', (event) => {
+        console.log(event);
+        const data = JSON.parse(event.data);
+        if (data.variable === 'phone_number') {
+            message.innerHTML = `A code has been sent to <strong>${data.phoneNumber}</strong> You
+                        should get it within 20 seconds`;
+        }
+    });
 });
