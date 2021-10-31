183cb8d5930d14f85ca1ed1f99ab564efff1b6e0
Chiedu <iamnedu912@gmail.com>
2021-10-31 09:05:16 +0100

    test

--- checkout/enter-the-code/index.js
+++ checkout/enter-the-code/index.js
@@ -1,9 +1,30 @@
 var message = document.getElementById('success-otp-alert');
+const goBackToVerifyUser = document.getElementById('verify-user');
+const closeBtn = document.getElementById('close-btn');
+
+closeBtn.addEventListener('click', () => {
+    window.top.postMessage(
+        JSON.stringify({
+            closeCheckoutId: 'verify-user',
+        }),
+        '*'
+    );
+});
+
 window.addEventListener('message', (event) => {
-    console.log(event);
     const data = JSON.parse(event.data);
     if (data.variable === 'phone_number') {
         message.innerHTML = `A code has been sent to <strong>${data.phoneNumber}</strong> You
                     should get it within 20 seconds`;
     }
+});
+
+goBackToVerifyUser.addEventListener('click', () => {
+    window.top.postMessage(
+        JSON.stringify({
+            closeCheckoutId: 'enter-the-code',
+            openCheckoutId: 'verify-user',
+        }),
+        '*'
+    );
 });
