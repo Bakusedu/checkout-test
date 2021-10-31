14561f778a033e8211a6b0323d8eb6e5bf95dca5
Chiedu <iamnedu912@gmail.com>
2021-10-31 07:04:58 +0100

    added more demo forms

--- checkout/enter-the-code/index.js
+++ checkout/enter-the-code/index.js
@@ -1,6 +1,7 @@
 var message = document.getElementById('success-otp-alert');
 
 window.addEventListener('message', (event) => {
+    console.log(event);
     const data = JSON.parse(event.data);
     if (data.variable === 'phone_number') {
         message.innerHTML = `A code has been sent to <strong>${data.phoneNumber}</strong> You
